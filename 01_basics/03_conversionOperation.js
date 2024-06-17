1dfed88b804e47b16c8a037bae1298ce5a0945f2
Deepanshu Singhal <117720451+itzdeepanshu30@users.noreply.github.com>
2024-06-17 11:13:16 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,19 +1,19 @@
-let score = "33"
-let score2="33abc"
+// let score = "33"
+// let score2="33abc"
 
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber=Number(score)
+// let valueInNumber=Number(score)
 
-let valueInNumber2=Number(score2)
+// let valueInNumber2=Number(score2)
 
-console.log(typeof valueInNumber);
-console.log( valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
 
 
-console.log(typeof valueInNumber2);
-console.log( valueInNumber2);
+// console.log(typeof valueInNumber2);
+// console.log( valueInNumber2);
 // "33"=>33
 //"33abc"=> NaN
 // true=> 1; false=>0
@@ -26,3 +26,34 @@ console.log( valueInNumber2);
 // "hitesh"=>true
 
 // for changeing in string use // string()
+
+
+// ****************************Operations***********************
+
+let v =3;
+let negValue=-v
+//console.log(negValue);
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2+2)
+// console.log(2%3)
+// let str ="hello 
+// let str2="Dev"
+// let str3= str+str2
+// console.log((str3));
+// console.log(("1"+2))
+// console.log((1+"2"))
+// console.log(typeof("1"+2))
+// console.log(typeof(1+"2"))
+// console.log(("1"+2+2))
+// console.log(typeof("1"+2+2))
+// console.log((1+"2"+2))
+// console.log(typeof(1+"2"+2))
+// console.log(("1"+2+2)) // 122
+// console.log((1+2+"2")) // 32
+// console.log(typeof(1+2+"2"))
+
+// prefix and postfix  leaning
