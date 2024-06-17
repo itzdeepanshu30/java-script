d0ed182b0277d9ba90a1628e608551c858bcb85f
Deepanshu Singhal <117720451+itzdeepanshu30@users.noreply.github.com>
2024-06-17 10:28:38 +0000

    conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33"
+let score2="33abc"
+
+
+console.log(typeof score);
+
+let valueInNumber=Number(score)
+
+let valueInNumber2=Number(score2)
+
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+
+console.log(typeof valueInNumber2);
+console.log( valueInNumber2);
+// "33"=>33
+//"33abc"=> NaN
+// true=> 1; false=>0
+// null=> 0
+// undefined=> NaN
+
+// for convesion into boolean we use //   Boolean()
+// 1=> true; 0 => false
+// ""=> false
+// "hitesh"=>true
+
+// for changeing in string use // string()
