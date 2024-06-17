1165e3a4f3aff0755fcd389de5a224de99486fce
Deepanshu Singhal <117720451+itzdeepanshu30@users.noreply.github.com>
2024-06-17 12:23:27 +0000

    summary

---
+++ 01_basics/dataTypeSummary.js
@@ -0,0 +1,30 @@
+//Return type of variables in JavaScript
+// Primitive Datatypes
+// Number => number
+// String  => string
+// Boolean  => boolean
+// null  => object
+// undefined  =>  undefined
+// Symbol  =>  symbol
+// BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+// Arrays  =>  object
+// Function  =>  function
+// Object  =>  object
+
+// java script is dymaically typed language
+// non primitive ka data type jo object (function object )
+// function ka data type function hi hai (function object)
+//Primitive datatypes:
+// String - string
+// Number - number
+// Boolean - Boolean
+// Null - object(****)
+// Undefined - undefined
+// BigInt - bigint
+
+// Non-primitive datatypes:
+// Array - object
+// Function - function(object)
+// Object - object
