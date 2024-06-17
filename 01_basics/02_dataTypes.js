eabd53846fe2d0f481317bbb25d92460fcf6ddcf
Deepanshu Singhal <117720451+itzdeepanshu30@users.noreply.github.com>
2024-06-17 10:12:55 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code ans newer version
+
+//alert(3+3) // we are using node js ,not browser
+
+// code readblity jarori hai
+// number =>2 to power 53
+// bigint 
+// string
+ //boolean
+ //null  // satndalone value
+ // undefined =>
+    // symbol => unique
+   
+ // objects
+  
+ console.log(typeof("Dev"));
+ console.log(typeof null);//object
+ console.log(typeof(undefined));// undefined
