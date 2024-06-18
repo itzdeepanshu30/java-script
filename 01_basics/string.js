7576e4aafc2fce65001394788a47b3af3d0ac89f
Deepanshu Singhal <117720451+itzdeepanshu30@users.noreply.github.com>
2024-06-18 05:23:44 +0000

    About String

---
+++ 01_basics/string.js
@@ -0,0 +1,316 @@
+// const name="dev "
+// const repoCount=50
+// console.log(name+repoCount);
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)// moder way and called string interpolation
+
+
+// const gameName= new String('hello')// 
+// //console.log(gameName)
+
+// const newString=gameName.substring(0,4);
+// console.log(newString)
+
+// const newStringOne="   Dev  "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+// const url ="https://dev.com/dev%20singhal"
+
+// console.log(url.replace('%20','-'))
+
+// console.log(url.includes('sundar'))
+
+
+const name2="dev-singhal-20"
+//console.log(name2.split('-'));
+//console.log(name2.blink());
+console.log(name2.padEnd(20,'.'));
+// const str1 = 'Breaded Mushrooms';
+
+// console.log(str1.padEnd(25, '.'));
+/*
+
+
+String {'hello'}
+0
+: 
+"h"
+1
+: 
+"e"
+2
+: 
+"l"
+3
+: 
+"l"
+4
+: 
+"o"
+length
+: 
+5
+[[Prototype]]
+: 
+String
+anchor
+: 
+ƒ anchor()
+at
+: 
+ƒ at()
+big
+: 
+ƒ big()
+blink
+: 
+ƒ blink()
+length
+: 
+0
+name
+: 
+"blink"
+arguments
+: 
+(...)
+caller
+: 
+(...)
+[[Prototype]]
+: 
+ƒ ()
+[[Scopes]]
+: 
+Scopes[0]
+bold
+: 
+ƒ bold()
+charAt
+: 
+ƒ charAt()
+charCodeAt
+: 
+ƒ charCodeAt()
+codePointAt
+: 
+ƒ codePointAt()
+concat
+: 
+ƒ concat()
+constructor
+: 
+ƒ String()
+endsWith
+: 
+ƒ endsWith()
+fixed
+: 
+ƒ fixed()
+fontcolor
+: 
+ƒ fontcolor()
+fontsize
+: 
+ƒ fontsize()
+includes
+: 
+ƒ includes()
+indexOf
+: 
+ƒ indexOf()
+isWellFormed
+: 
+ƒ isWellFormed()
+italics
+: 
+ƒ italics()
+lastIndexOf
+: 
+ƒ lastIndexOf()
+length
+: 
+0
+link
+: 
+ƒ link()
+localeCompare
+: 
+ƒ localeCompare()
+length
+: 
+1
+name
+: 
+"localeCompare"
+arguments
+: 
+(...)
+caller
+: 
+(...)
+[[Prototype]]
+: 
+ƒ ()
+[[Scopes]]
+: 
+Scopes[0]
+match
+: 
+ƒ match()
+length
+: 
+1
+name
+: 
+"match"
+arguments
+: 
+(...)
+caller
+: 
+(...)
+[[Prototype]]
+: 
+ƒ ()
+[[Scopes]]
+: 
+Scopes[0]
+matchAll
+: 
+ƒ matchAll()
+normalize
+: 
+ƒ normalize()
+padEnd
+: 
+ƒ padEnd()
+length
+: 
+1
+name
+: 
+"padEnd"
+arguments
+: 
+(...)
+caller
+: 
+(...)
+[[Prototype]]
+: 
+ƒ ()
+[[Scopes]]
+: 
+Scopes[0]
+padStart
+: 
+ƒ padStart()
+repeat
+: 
+ƒ repeat()
+replace
+: 
+ƒ replace()
+replaceAll
+: 
+ƒ replaceAll()
+search
+: 
+ƒ search()
+slice
+: 
+ƒ slice()
+length
+: 
+2
+name
+: 
+"slice"
+arguments
+: 
+(...)
+caller
+: 
+(...)
+[[Prototype]]
+: 
+ƒ ()
+[[Scopes]]
+: 
+Scopes[0]
+small
+: 
+ƒ small()
+split
+: 
+ƒ split()
+startsWith
+: 
+ƒ startsWith()
+strike
+: 
+ƒ strike()
+sub
+: 
+ƒ sub()
+substr
+: 
+ƒ substr()
+substring
+: 
+ƒ substring()
+sup
+: 
+ƒ sup()
+toLocaleLowerCase
+: 
+ƒ toLocaleLowerCase()
+toLocaleUpperCase
+: 
+ƒ toLocaleUpperCase()
+toLowerCase
+: 
+ƒ toLowerCase()
+toString
+: 
+ƒ toString()
+toUpperCase
+: 
+ƒ toUpperCase()
+toWellFormed
+: 
+ƒ toWellFormed()
+trim
+: 
+ƒ trim()
+trimEnd
+: 
+ƒ trimEnd()
+trimLeft
+: 
+ƒ trimStart()
+trimRight
+: 
+ƒ trimEnd()
+trimStart
+: 
+ƒ trimStart()
+valueOf
+: 
+ƒ valueOf()
+Symbol(Symbol.iterator)
+: 
+ƒ [Symbol.iterator]()
+[[Prototype]]
+: 
+Object
+[[PrimitiveValue]]
+: 
+""
+[[PrimitiveValue]]
+: 
+"hello"
+
+*/
