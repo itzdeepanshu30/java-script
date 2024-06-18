028031232b5c282f5ee32ef1fb3b8ddab251e4e7
Deepanshu Singhal <117720451+itzdeepanshu30@users.noreply.github.com>
2024-06-18 06:26:15 +0000

    Math And Num

---
+++ 01_basics/05_nums_and_maths.js
@@ -0,0 +1,41 @@
+// const score=400
+
+// const balance =  new Number(100)
+// console.log(balance)
+// console.log(balance.toString())
+// console.log(balance.toFixed(2))
+
+
+// const otherNumber=123.8987
+// console.log((otherNumber.toPrecision(4)));
+
+
+// const hundreds=1000000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++  Maths  ++++++++++++++++++++++++
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.3))
+
+// console.log(Math.ceil(4.2))
+
+// console.log(Math.floor(4.2))
+// console.log(Math.min(4,3,5,8,1))
+
+// important
+
+
+//console.log(Math.round(Math.random()*10)+1); // range between to zero to 1 and zero and 1 is also includes for random function
+
+
+ 
+const min =10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
+
+
+
