a451fad925b6cdf54573805035475aa99bb8e125
Deepanshu Singhal <117720451+itzdeepanshu30@users.noreply.github.com>
2024-06-21 17:42:48 +0000

    object part2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,33 @@
+//const tinderUser=new Object() // singleotn object
+ 
+const tinderUser ={}
+ tinderUser.id="de23"
+ tinderUser.name="Deepanshu"
+
+//console.log(tinderUser);
+
+const regularUser={
+    email:"Somil@gamil.com",
+    fullname: {
+       userfullname: {
+        firstnmae:"Deepanshu",
+        lastname:"Singhal"
+       }
+    }
+}
+//console.log(regularUser.fullname.userfullname.lastname)
+const obj1={1:"a",2:"b",3:"c"}
+const obj2= {3:"a",4:"b"}
+//const obj3={obj1,obj2}
+//console.log(obj3)
+
+// const obj3=Object.assign({},obj1,obj2) 
+// console.log(obj3)
+
+const obj3={...obj1,...obj2}
+ console.log(obj3)
+
+
+
+ console.log(Object.keys(tinderUser))// important
+ console.log(tinderUser.hasOwnProperty('id'))
