a6ae0e8291ec5e26da91d67cb817a12cc21f0efd
Deepanshu Singhal <117720451+itzdeepanshu30@users.noreply.github.com>
2024-06-22 14:57:55 +0000

    scope part1

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,12 @@
+ //var c =300
+ let a =300
+ {
+    let a=10
+    const b=20           
+    //var c =3
+    console.log("INNER:",a)
+ }
+ console.log(a);
+ //console.log(b);
+ //console.log(c);
+  // global scope is differnt in node or inspect // imp for interview
