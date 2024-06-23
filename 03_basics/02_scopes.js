c940fd1f74d30b8e659651ab7e9aa7747557b43a
Deepanshu Singhal <117720451+itzdeepanshu30@users.noreply.github.com>
2024-06-23 05:25:56 +0000

    scopes part2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,9 +4,34 @@
     let a=10
     const b=20           
     //var c =3
-    console.log("INNER:",a)
+   // console.log("INNER:",a)
  }
- console.log(a);
+ //console.log(a);
  //console.log(b);
  //console.log(c);
   // global scope is differnt in node or inspect // imp for interview
+
+
+  function one(){
+   const username="hitesh"
+   function two(){
+      const  website="youtube"
+     console.log(username); 
+   }
+  // console.log(website)
+   two()
+  }
+ // one()
+
+
+  ///////////////////////////////////////////
+  addOne(5) // yaha par koi error nhi aayega
+  function addOne(num){
+   return num+1
+  }
+ // addtwo(6) // yaha error aayga
+
+  const addtwo=function(num){
+   return num+1
+  }
+  addtwo
