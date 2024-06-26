d1f6c72261853aa85070070e66c4aa28fd62c652
Deepanshu Singhal <117720451+itzdeepanshu30@users.noreply.github.com>
2024-06-26 07:08:09 +0000

    loops

---
+++ 05_iterations/loops.js
@@ -0,0 +1,18 @@
+// for
+
+// for (let i = 0; i < 10; i++) {
+//     console.log (i)
+    
+// }
+//
+
+//break  and continue;
+
+let index =0
+while(index<=10){
+    console.log(index)
+     index=index+2;
+}
+do{
+
+} while(condition)
