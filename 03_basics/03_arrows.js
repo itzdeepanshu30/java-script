0dd0e15b9c9513805766ca5c8059e238e42c1b05
itzdeepanshu30 <deepanshusinghal509@gmail.com>
2025-02-25 14:26:40 +0530

    learning

--- 03_basics/03_arrows.js
+++ 03_basics/03_arrows.js
@@ -9,25 +9,26 @@
 
 
 // }
-//  user.welcomeMessage();
+//   user.welcomeMessage();
 //  user.username="sam";
 // user.welcomeMessage();
-// console.log(this);// yaha  par ye hai Empty object {}// but browser par global jo hai vo windows hai
+//  console.log(this);// yaha  par ye hai Empty object {}// but browser par global jo hai vo windows hai
 
 // function chai(){
-//     //let username="dev"
+    //let username="dev"
+
   // console.log(this)   // yaha par this empty object nhi aayega
 
-//     console.log(this.username);   // function mai this ko use nhi krte 
+    // console.log(this.username);   // function mai this ko use nhi krte 
 
 // }
 // chai();
 
 
-const chai=()=>{                  // this is arrow function
-    let username="Deepanshu"
-    console.log(this)
-}
+// const chai=()=>{                  // this is arrow function
+//     let username="Deepanshu"
+//     console.log(this)
+// }
 //chai();     // yaha par this hai jo empty object aayega 
 
 
@@ -36,11 +37,11 @@ const chai=()=>{                  // this is arrow function
 // }
  //const addTwo=(num1,num2) => num1+num2 // implicit return ka tarika 
 //  const addTwo=(num1,num2) => (num1+num2) // agar curly barckets use kroge toh return likna pageda
-const addTwo=(num1,num2) => ({user:"deep"})
+// const addTwo=(num1,num2) => ({user:"deep"})
 
 
-console.log(addTwo(3,4))
+// console.log(addTwo(3,4))
 
 
-const myArray=[2,3,4,5,6,7]
+// const myArray=[2,3,4,5,6,7]
 //myArray.forEach()
